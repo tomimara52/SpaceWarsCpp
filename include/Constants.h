1d8625650fb5a55355b30112caee49782334c45b
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-07-11 15:37:17 -0300

    use spaces for indentation

--- include/Constants.h
+++ include/Constants.h
@@ -2,9 +2,9 @@
 
 #include <cstdint>
 
-const uint8_t FORWARD =			0b00000001;
-const uint8_t STOP_FORWARD =	0b00000010;
-const uint8_t RESET_VEL =		0b00000100;
+const uint8_t FORWARD =         0b00000001;
+const uint8_t STOP_FORWARD =    0b00000010;
+const uint8_t RESET_VEL =       0b00000100;
 
 const double ACCELERATION = 80.0;
 const double MIN_SPEED = 40.0;
