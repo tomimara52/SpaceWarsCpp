f9b71bb6e5d08490a8862ae55bcfc128cea6fe18
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-07-17 15:06:39 -0300

    set player id to 'p'

--- src/player.cpp
+++ src/player.cpp
@@ -8,7 +8,7 @@
 #include "Math.h"
 
 Player::Player(double p_x, double p_y, double dir, SDL_Texture *tex)
-	: Entity(p_x, p_y, tex), dir{ dir }, momentum{}, events{} { }
+	: Entity(p_x, p_y, tex, 'p'), dir{ dir }, momentum{}, events{} { }
 
 void Player::setEvents(uint_fast8_t newEvents) {
 	events = newEvents;
