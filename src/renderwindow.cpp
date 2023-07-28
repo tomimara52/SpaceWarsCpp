ece3c4245c39e84ce91a49cbc0a7cdc3b879773f
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-07-28 19:41:54 -0300

    start window maximized

--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -11,7 +11,7 @@
 RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
 	: window{ NULL }, renderer{ NULL } {
 
-	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
 
 	if (window == NULL)
 		std::cout << "Window failed to initialize with error: "
